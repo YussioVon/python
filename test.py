2c1505582c7778ad1401f150c7da7af69087262e
fyx <fyx98628@gmail.com>
2021-04-21 20:52:03 +0800

    修改服务器信息获取方式

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 
 
 
-
+'''
 def conn(self):
     serverIp = ['12.99.105.184','12.99.105.185']
     serverPwd = ['123456','nbcb2017']
+
+    for i,p in serverIp,serverPwd:
+        connect(serverIp[[i],serverPwd[p])
+'''
+serverIp = ['12.99.105.184','12.99.105.185']
+serverPwd = ['123456','nbcb2017']
+serverInfo = [('12.99.105.184','123456'),('12.99.105.185','nbcb2017')]
+for  i,p in serverIp,serverPwd:
+        print(i+"and"+ p+'\n')
+for all in serverInfo:
+    print(all[0]+all[1])
